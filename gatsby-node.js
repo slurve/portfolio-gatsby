5a83d4b2bafa9f6f047e0388322d5159538d1bbb
Tom Rose <tom@slurve.com>
2020-02-13 11:04:56 -0500

    comment out blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,28 +1,28 @@
-const path = require(`path`)
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-  const result = await graphql(`
-    query {
-      wp {
-        posts {
-          edges {
-            node {
-              content
-              title
-              slug
-              excerpt
-            }
-          }
-        }
-      }
-    }
-  `)
+// const path = require(`path`)
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const result = await graphql(`
+//     query {
+//       wp {
+//         posts {
+//           edges {
+//             node {
+//               content
+//               title
+//               slug
+//               excerpt
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  result.data.wp.posts.edges.forEach(({ node }) => {
-    createPage({
-      path: node.slug,
-      component: path.resolve(`./src/templates/post.js`),
-      context: { node },
-    })
-  })
-}
+//   result.data.wp.posts.edges.forEach(({ node }) => {
+//     createPage({
+//       path: node.slug,
+//       component: path.resolve(`./src/templates/post.js`),
+//       context: { node },
+//     })
+//   })
+// }
