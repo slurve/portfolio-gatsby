6c0b8403049b05629f5659d3449596faf67cea9a
Tom Rose <tom@slurve.com>
2020-05-11 21:27:02 -0400

    temp remove offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
     `gatsby-plugin-sitemap`,
   ],
 }
