a1899c3de621eee5503c62518e9146ef6cc30bcc
Tom Rose <tom@slurve.com>
2022-03-14 20:31:39 -0400

    update avail

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -101,9 +101,8 @@ const Sidebar = ({ version }) => {
         <img src={ubahn} alt="Tom Rose" />
         <h2>Availability</h2>
         <p className="availability">
-          <span className="unavailable">JANUARY: Unavailable</span><br />
-          <span className="unavailable">FEBRUARY: Unavailable</span><br />
-          <span className="available">MARCH+: Available</span>
+          <span className="limited">MARCH: Limited</span><br />
+          <span className="available">APRIL+: Available</span>
         </p>
         <p>I'd love to set up a time to hear about your project. Let's talk!</p>
         <a href="/contact" className="button">
