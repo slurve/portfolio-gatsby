ca207be3b1c5603a627b2695662f056344767816
Tom Rose <tom@slurve.com>
2023-01-06 15:05:35 -0500

    add overrides

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,20 +1,20 @@
-// import React from "react"
-// import Layout from "../components/layout"
-// import SEO from "../components/seo"
-// const BlogPost = blogPost => {
-//   const postTitle = blogPost.pageContext.node.title
-//   const postContent = blogPost.pageContext.node.content
-//   const postDate = blogPost.pageContext.node.date
-//   return (
-//     <Layout type="split" slug="blog">
-//       <SEO title="blog" />
-//       <>
-//         <h1>{postTitle}</h1>
-//         <p class="meta">Published on {postDate} by Tom Rose</p>
-//         <span dangerouslySetInnerHTML={{ __html: postContent }} />
-//       </>
-//     </Layout>
-//   )
-// }
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+const BlogPost = blogPost => {
+  const postTitle = blogPost.pageContext.node.title
+  const postContent = blogPost.pageContext.node.content
+  const postDate = blogPost.pageContext.node.date
+  return (
+    <Layout type="split" slug="blog">
+      <SEO title="blog" />
+      <>
+        <h1>{postTitle}</h1>
+        <p class="meta">Published on {postDate} by Tom Rose</p>
+        <span dangerouslySetInnerHTML={{ __html: postContent }} />
+      </>
+    </Layout>
+  )
+}
 
-// export default BlogPost
+export default BlogPost
